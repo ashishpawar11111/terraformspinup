48f0e91ec074c769c924c5ffa653dfa1282b76c8
ashishpawar11111 <ashishpawar11111@gmail.com>
2026-05-18 13:03:25 +0530

    Created VmRSS script adding status dir

--- Scripts/RSSalerts.sh
+++ Scripts/RSSalerts.sh
@@ -4,7 +4,7 @@ THRESHOLD=$1
 PID=$2
 LOGFILE=/var/log/rss_alerts.log
 while true; do
-    RSS=$(grep VmRSS $PID | awk '{print $2}')
+    RSS=$(grep VmRSS /proc/$PID/status | awk '{print $2}')
     if [ "$RSS" -gt "$THRESHOLD" ]; then
         echo "Memory usage exceeded threshold: $RSS KB" >> "$LOGFILE"
         echo "Memory usage exceeded threshold: $RSS KB" | mail -s "RSS Alert" "$ALERT_EMAIL"
